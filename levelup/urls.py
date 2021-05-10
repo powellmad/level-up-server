b1234d9ff60587de5a793e329f3dd3f9cd22885b
powellmad <madsnpowell@gmail.com>
2021-05-10 09:38:09 -0500

    added views for gametype

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views import GameTypes
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypes, 'gametype')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
