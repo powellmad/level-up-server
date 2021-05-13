e42b520aafb49ccb48cc26b368376404b5d7a480
powellmad <madsnpowell@gmail.com>
2021-05-13 10:51:12 -0500

    edited by event fixtures and model to match up

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Event(models.Model):
     description = models.TextField()
-    start_date = models.DateTimeField()
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    time = models.DateTimeField()
     organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="attending")
