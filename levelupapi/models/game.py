fd02c528c4772b8864486df6463359be47149ae9
powellmad <madsnpowell@gmail.com>
2021-05-14 09:37:51 -0500

    all games render on the dom

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,4 +4,4 @@ class Game(models.Model):
     title = models.CharField(max_length=50)
     instructions = models.TextField()
     max_players = models.IntegerField()
-    type = models.ForeignKey('GameType', on_delete=models.CASCADE) 
+    gametype = models.ForeignKey('GameType', on_delete=models.CASCADE) 
